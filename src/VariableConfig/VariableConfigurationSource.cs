20f9b29ab37560997b8c568bcfac8d921fd49461
Nathan Ellenfield <nathan.ellenfield@gmail.com>
2018-06-21 17:14:40 -0400

    Adjusted target frameworks to build for .netcore 2.0 and .net framework 4.7. Added license headers. Added documentation. Added nuget package. Slight updates to readme.

--- src/VariableConfig/VariableConfigurationSource.cs
+++ src/VariableConfig/VariableConfigurationSource.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
+﻿// Copyright (c) Nathan Ellenfield. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.Extensions.Configuration;
 
 namespace VariableConfig
 {
+    /// <summary>
+    /// Represents data in an <see cref="IConfiguration"/> as an <see cref="IConfigurationSource"/>.
+    /// </summary>
     public class VariableConfigurationSource : IConfigurationSource
     {
+        /// <summary>
+        /// The underlying configuration.
+        /// </summary>
         public IConfiguration Configuration { get; set; }
 
+        /// <summary>
+        /// Builds the <see cref="VariableConfigurationProvider"/> for this source.
+        /// </summary>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/>.</param>
+        /// <returns>A <see cref="VariableConfigurationProvider"/></returns>
         public IConfigurationProvider Build(IConfigurationBuilder builder) => new VariableConfigurationProvider(this);
     }
 }
