13c8d4737a2a148d4c86341dcb0bc2a0b03d807d
Nathan Ellenfield <nathan.ellenfield@gmail.com>
2018-06-22 18:11:03 -0400

    Added license headers to json tests.

--- test/VariableConfig.Test/json/JsonEscaping.cs
+++ test/VariableConfig.Test/json/JsonEscaping.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿// Copyright (c) Nathan Ellenfield. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 
