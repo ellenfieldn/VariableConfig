3b0365fedcaeaaa27a0d7b9e52fd65ade43cbc14
Nathan Ellenfield <nathan.ellenfield@gmail.com>
2018-06-22 18:10:01 -0400

    Added test for json config. Added note to readme about json. Small cleanup.

---
+++ test/VariableConfig.Test/json/JsonEscaping.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace VariableConfig.Test.json
+{
+    [TestClass, TestCategory("Unit Tests"), TestCategory("Acceptance Tests"), TestCategory("Escaping")]
+    public class JsonEscaping : Escaping
+    {
+        public static new IConfiguration Configuration { get; set; }
+
+        public JsonEscaping()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("json/escaping.json")
+                .AddVariableConfiguration();
+            Configuration = builder.Build();
+        }
+    }
+}
---
+++ test/VariableConfig.Test/json/JsonVariables.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace VariableConfig.Test.json
+{
+    [TestClass, TestCategory("Unit Tests"), TestCategory("Acceptance Tests"), TestCategory("Variables")]
+    public class JsonVariables : Variables
+    {
+        public static new IConfiguration Configuration { get; set; }
+
+        public JsonVariables()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("json/variables.json")
+                .AddVariableConfiguration();
+            Configuration = builder.Build();
+        }
+    }
+}
